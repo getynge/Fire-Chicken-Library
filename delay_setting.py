65ffb63c1cf60206f83d4bca4d3c86d676797462
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-08-15 20:45:53 -0600

    Add files via upload

---
+++ delay_setting.py
@@ -0,0 +1,16 @@
+from talon import actions, settings, Module
+
+class DelaySetting:
+    def __init__(self, name: str, default_amount: int, description: str, units: str = 'ms'):
+        module = Module()
+        self.setting = module.setting(
+            name,
+            type = int,
+            default = default_amount,
+            desc = f'How much to pause when {description}. If any commands using this setting are not working, try increasing this.'
+        )
+        self.units = units
+
+    def sleep(self):
+        actions.sleep(f'{self.setting.get()}' + self.units)
+    
