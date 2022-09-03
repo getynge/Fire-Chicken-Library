55a0f45bfce68532fc4a7a08271ac4c7c41a29d8
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-09-03 16:54:55 -0600

    Make static methods class methods for readability

--- fire_chicken/data_storage.py
+++ fire_chicken/data_storage.py
@@ -165,24 +165,24 @@ class JSONConverter:
 class ObjectFromJSONConverter:
     def __init__(self, object_from_json):
         self.object_from_json = self._get_from_json_function(object_from_json)
-    @staticmethod
-    def _get_from_json_function(from_json):
-        if ObjectFromJSONConverter._from_json_function_is_method(from_json):
-            return ObjectFromJSONConverter._get_from_json_function_from_class(from_json)
+    @classmethod
+    def _get_from_json_function(cls, from_json):
+        if cls._from_json_function_is_method(from_json):
+            return cls._get_from_json_function_from_class(from_json)
         else:
             return from_json
-    @staticmethod
-    def _get_from_json_function_from_class(classname):
+    @classmethod
+    def _get_from_json_function_from_class(cls, classname):
         return lambda value : classname.from_json(value)
-    @staticmethod
-    def _from_json_function_is_method(from_json):
-        return ObjectFromJSONConverter._has_from_json_attribute(from_json) \
-           and ObjectFromJSONConverter._has_callable_from_json_attribute(from_json)
-    @staticmethod
-    def _has_from_json_attribute(from_json):
+    @classmethod
+    def _from_json_function_is_method(cls, from_json):
+        return cls._has_from_json_attribute(from_json) \
+           and cls._has_callable_from_json_attribute(from_json)
+    @classmethod
+    def _has_from_json_attribute(cls, from_json):
         return hasattr(from_json, 'from_json')
-    @staticmethod
-    def _has_callable_from_json_attribute(from_json):
+    @classmethod
+    def _has_callable_from_json_attribute(cls, from_json):
         return callable(from_json.from_json)
 
     def convert_json(self, text):
@@ -190,31 +190,31 @@ class ObjectFromJSONConverter:
         if _value_unavailable(self.object_from_json):
             return json_value
         return self.object_from_json(json_value)
-    @staticmethod
-    def _convert_json_using_default_decoding(text):
+    @classmethod
+    def _convert_json_using_default_decoding(cls, text):
         return json.loads(text)
 
 class JSONFromObjectConverter:
     def __init__(self, *, to_json_function, to_json_class):
         self.json_from_object = self._get_json_from_object_function(to_json_function, to_json_class)
 
-    @staticmethod
-    def _get_json_from_object_function(to_json_function, to_json_class):
-        JSONFromObjectConverter._raise_exception_if_invalid_json_from_object_argument_combination(to_json_function, to_json_class)
+    @classmethod
+    def _get_json_from_object_function(cls, to_json_function, to_json_class):
+        cls._raise_exception_if_invalid_json_from_object_argument_combination(to_json_function, to_json_class)
         if _value_provided(to_json_function):
-            return JSONFromObjectConverter._get_json_from_object_function_using_converter_function(to_json_function)
+            return cls._get_json_from_object_function_using_converter_function(to_json_function)
         if _value_provided(to_json_class):
-            return JSONFromObjectConverter._get_json_from_object_function_using_converter_class(to_json_class)
+            return cls._get_json_from_object_function_using_converter_class(to_json_class)
         return None
-    @staticmethod
-    def _raise_exception_if_invalid_json_from_object_argument_combination(to_json_function, to_json_class):
+    @classmethod
+    def _raise_exception_if_invalid_json_from_object_argument_combination(cls, to_json_function, to_json_class):
         if _values_provided(to_json_function, to_json_class):
             raise ValueError('JSONFile objects should not receive default and cls')
-    @staticmethod
-    def _get_json_from_object_function_using_converter_function(function):
+    @classmethod
+    def _get_json_from_object_function_using_converter_function(cls, function):
         return lambda value : json.dumps(value, default = function)
-    @staticmethod
-    def _get_json_from_object_function_using_converter_class(converter_class):
+    @classmethod
+    def _get_json_from_object_function_using_converter_class(cls, converter_class):
         return lambda value : json.dumps(value, cls = converter_class)
 
     def convert_object(self, value) -> str:
@@ -223,21 +223,22 @@ class JSONFromObjectConverter:
         if self._value_has_encoder_method(value):
             return self._encode_using_encoder_method(value)
         return self._encode_using_json_default_encoding(value)
-    @staticmethod
-    def _value_has_encoder_method(value):
-        return JSONFromObjectConverter._value_has_to_json_attribute(value) \
-           and JSONFromObjectConverter._value_has_callable_to_json_attribute(value)
-    @staticmethod
-    def _value_has_to_json_attribute(value):
+    @classmethod
+    def _value_has_encoder_method(cls, value):
+        return cls._value_has_to_json_attribute(value) \
+           and cls._value_has_callable_to_json_attribute(value)
+    @classmethod
+    def _value_has_to_json_attribute(cls, value):
         return hasattr(value, 'to_json')
-    def _value_has_callable_to_json_attribute(value):
+    @classmethod
+    def _value_has_callable_to_json_attribute(cls, value):
         return callable(value.to_json)
-    @staticmethod
-    def _encode_using_encoder_method(value):
+    @classmethod
+    def _encode_using_encoder_method(cls, value):
         json_object = value.to_json()
-        return JSONFromObjectConverter._encode_using_json_default_encoding(json_object)
-    @staticmethod
-    def _encode_using_json_default_encoding(value):
+        return cls._encode_using_json_default_encoding(json_object)
+    @classmethod
+    def _encode_using_json_default_encoding(cls, value):
         return json.dumps(value)
 
 def _value_provided(value):
