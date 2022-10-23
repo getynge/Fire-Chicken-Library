a9bc351a9c24e730de4b5e101e05a5be9055ae14
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-10-22 19:22:17 -0600

    Change sleep setting to work with floats

--- fire_chicken/sleep_utilities.py
+++ fire_chicken/sleep_utilities.py
@@ -1,19 +1,17 @@
 from talon import actions, Module
 
 class SleepSetting:
-    def __init__(self, name: str, default_amount: int, description: str, units: str = 'ms'):
+    def __init__(self, name: str, default_amount: float, description: str):
         module = Module()
         self.setting = module.setting(
             name,
-            type = int,
+            type = float,
             default = default_amount,
             desc = f'How much to pause when {description}. If any commands using this setting are not working, try increasing this.'
         )
-        self.units = units
 
     def sleep(self):
-        actions.sleep(f'{self.get()}' + self.units)
+        actions.sleep(self.get())
     
     def get(self):
         return self.setting.get()
-
