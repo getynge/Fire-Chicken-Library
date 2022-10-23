4057697584bc14ec5eb938e0cda9ddfe2a763bc9
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2022-10-22 19:28:49 -0600

    Add sleep maximum setting function

--- fire_chicken/sleep_utilities.py
+++ fire_chicken/sleep_utilities.py
@@ -15,3 +15,10 @@ class SleepSetting:
     
     def get(self):
         return self.setting.get()
+
+def sleep_max_setting(* args):
+    maximum_delay_amount = 0
+    for setting in args:
+        if setting.get() > maximum_delay_amount:
+            maximum_delay_amount = setting.get()
+    actions.sleep(maximum_delay_amount)
