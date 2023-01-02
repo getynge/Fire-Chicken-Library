2e54741daec32c5c0e4495b1d8c0a0e0b48df456
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-01 19:34:48 -0700

    Add prefix and postfix functions

--- fire_chicken/tag_utilities.py
+++ fire_chicken/tag_utilities.py
@@ -12,3 +12,14 @@ def deactivate_tags_in_context(context):
 
 def make_tag_only_active_tag_in_context(tag_name: str, context):
     context.tags = [tag_name]
+
+def compute_name_dot_index(tag_name: str) -> int:
+    return tag_name.find('.')
+
+def compute_postfix(tag_name: str) -> str:
+    dot_index = compute_name_dot_index(tag_name)
+    return tag_name[dot_index + 1:]
+
+def compute_prefix(tag_name: str):
+    dot_index = compute_name_dot_index(tag_name)
+    return tag_name[:dot_index]
