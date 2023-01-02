1c13d3763ed588d79d20ad51c745e3d7ea4cb6ff
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-01 18:08:01 -0700

    Remove manager support, Add prefix support
    Remove the dispatcher object being used to allow users to activate and deactivate switch tags directly through actions. Use the user prefix if no prefix provided in tag name, and provide ability to get the postfix and prefix.

--- fire_chicken/switch_tag.py
+++ fire_chicken/switch_tag.py
@@ -1,26 +1,19 @@
-from talon import Module, Context, actions, app
-
+from talon import Module, Context
+from .tag_utilities import compute_tag_name
 
 class SwitchTag:
     def __init__(self, name: str, description: str):
         module = Module()
         module.tag(name, description)
-        self.name = name
+        self.name = compute_tag_name(name)
         self.context = Context()
-        app.register('ready', self._register)
-    def _register(self):
-        try:
-            actions.user.switch_tag_insert(self)
-        except Exception as exception:
-            print('Switch tag manager not available', exception)
+
     def on(self):
-        self.context.tags = [self.get_name()]
-    def activate(self):
-        self.on()
+        self.context.tags = [self.name]
+
     def off(self):
         self.context.tags = []
-    def deactivate(self):
-        self.off()
+
     def switch(self):
         if len(self.context.tags) == 0:
             self.on()
@@ -28,6 +21,15 @@ class SwitchTag:
             self.off()
 
     def get_name(self) -> str:
-        return 'user.' + self.name
-    def get_postfix(self) -> str:
         return self.name
+    
+    def _get_dot_index(self):
+        return self.name.find('.')
+
+    def get_postfix(self) -> str:
+        dot_index = self._get_dot_index()
+        return self.name[dot_index + 1:]
+
+    def get_prefix(self) -> str:
+        dot_index = self._get_dot_index()
+        return self.name[:dot_index]
