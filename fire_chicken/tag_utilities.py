2583a1d6ca33c30e29e26dbd496d1f4711c78fbf
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-01-01 18:51:24 -0700

    Add tag activation and deactivation functions

--- fire_chicken/tag_utilities.py
+++ fire_chicken/tag_utilities.py
@@ -6,3 +6,9 @@ def compute_tag_name(partial_name: str):
     
 def has_prefix(text: str):
     return text.find('.') > 0
+
+def deactivate_tags_in_context(context):
+    context.tags = []
+
+def make_tag_only_active_tag_in_context(tag_name: str, context):
+    context.tags = [tag_name]
